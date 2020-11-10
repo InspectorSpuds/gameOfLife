68a1755f879bf232b5ff219930c567def3bc8b61
Ishan Parikh <ishanparikh18@gmail.com>
2020-11-09 19:56:26 -0800

    Basic click seeding functionality added

---
+++ src/sample/Board.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.scene.layout.GridPane;
+
+import java.util.HashSet;
+import java.util.concurrent.BlockingQueue;
+
+
+/*
+    class Board
+    Purpose: represents the board state for the game of life
+*/
+public class Board implements Runnable{
+    private BlockingQueue<HashSet<Square>> stateUpdates;
+    private HashSet<Square> nextState;
+    public final int waitTime = 5;
+    @Override
+    public void run() {
+
+        try {
+            Thread.sleep(1000*waitTime);
+        } catch (InterruptedException e) {
+            System.out.println("Thread wait is blocked");
+        }
+    }
+    public void calculate() {
+
+    }
+}
---
+++ src/sample/Square.java
@@ -0,0 +1,10 @@
+package sample;
+
+import javafx.scene.shape.Rectangle;
+
+public class Square extends Rectangle {
+
+    public Square(int width, int height) {
+        super(width,height);
+    }
+}
