67936b3688b0655ed19d9db2a3111d494a7e4e34
Ishan Parikh <ishanparikh18@gmail.com>
2020-12-17 14:49:50 -0800

    Alteration

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-/********************************
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *******************************/
-
-
 
 
 /*
